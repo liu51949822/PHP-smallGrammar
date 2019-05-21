9704ea4852030dca94bd6012bff0fbd6d2679879
liu51949822 <1240332437@qq.com>
2019-05-21 17:00:13 +0800

    更新

--- tp5/application/route.php
+++ tp5/application/route.php
@@ -8,14 +8,21 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+//配置狮
+//return [
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+//
+//];
+//动态路由
+use  think\Route;
+//Route::rule('hello','simple/Test/hello','post');
+Route::post('hello','simple/Test/hello');
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
 
-];
+
---
+++ tp5/application/simple/controller/Test.php
@@ -0,0 +1,17 @@
+<?php
+//命名空间
+namespace  app\simple\controller;
+use think\Request;
+class Test{
+ public  function  hello(){
+//     $id= Request::instance()->param('id');
+//     $name = Request::instance()->param('name');
+$all =  Request::instance()->param();
+var_dump($all);
+//     echo $id;
+//     echo '|';
+//     echo $name;
+//     echo 'hello,moto';
+ }
+
+}
